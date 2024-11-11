7519bb065138a47fea512fa49504a7696b403958
RayanEreteo <rayabf5@gmail.com>
2024-11-11 18:11:59 +0100

    Refactor du code pour le rrerndrre plus logic

---
+++ background.js
@@ -0,0 +1,30 @@
+const forbiddenWebsites = [
+    /youtube\.com\/(.*)/,
+    /twitter\.com\/(.*)/,
+    /instagram\.com\/(.*)/,
+    /tiktok\.com\/(.*)/,
+    /facebook\.com\/(.*)/,
+  ];
+  
+  function check(){
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let currentTab = tabs[0];
+      let currentUrl = currentTab.url;
+  
+      for (let i = 0; i < forbiddenWebsites.length; i++) {
+        if (forbiddenWebsites[i].test(currentUrl)) {
+          chrome.tabs.update({url: './block_page.html'})
+          break;
+        }
+      }
+    });
+  }
+  
+  function start() {
+    setInterval(check, 1000);     
+  }
+  
+  chrome.runtime.onStartup.addListener(function() {
+    start();
+  })
+  start();
---
+++ popup.js
@@ -0,0 +1,4 @@
+let shouldBlock = true
+
+let blockStatusText = document.querySelector("#block-status")
+let shouldBlockCheck = document.querySelector("#should-block")
