82d000f994b4493288b1e61e8a9a5f1793e265ca
RayanEreteo <rayabf5@gmail.com>
2024-11-11 20:17:22 +0100

    message envoyee

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
-let shouldBlock = true
-
 let blockStatusText = document.querySelector("#block-status")
-let shouldBlockCheck = document.querySelector("#should-block")
+let blockCheckbox = document.querySelector("#should-block")
+
+if(blockCheckbox){
+  blockCheckbox.addEventListener("change", (e) => {
+    chrome.runtime.sendMessage({ action: "toggleBlock", shouldBlock: e.target.checked });
+  })
+}
