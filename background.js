d55690e719ce3afd1a1e975efa4a04e175874a69
RayanEreteo <rayabf5@gmail.com>
2024-11-11 20:17:04 +0100

    message recus.

--- background.js
+++ background.js
@@ -1,30 +1,39 @@
 const forbiddenWebsites = [
-    /youtube\.com\/(.*)/,
-    /twitter\.com\/(.*)/,
-    /instagram\.com\/(.*)/,
-    /tiktok\.com\/(.*)/,
-    /facebook\.com\/(.*)/,
-  ];
-  
-  function check(){
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      let currentTab = tabs[0];
-      let currentUrl = currentTab.url;
-  
-      for (let i = 0; i < forbiddenWebsites.length; i++) {
-        if (forbiddenWebsites[i].test(currentUrl)) {
-          chrome.tabs.update({url: './block_page.html'})
-          break;
-        }
+  /youtube\.com\/(.*)/,
+  /twitter\.com\/(.*)/,
+  /instagram\.com\/(.*)/,
+  /tiktok\.com\/(.*)/,
+  /facebook\.com\/(.*)/,
+];
+
+let shouldBlock = true;
+
+function check() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let currentTab = tabs[0];
+    let currentUrl = currentTab.url;
+
+    for (let i = 0; i < forbiddenWebsites.length; i++) {
+      if (forbiddenWebsites[i].test(currentUrl)) {
+        chrome.tabs.update({ url: './block_page.html' })
+        break;
       }
-    });
-  }
-  
-  function start() {
-    setInterval(check, 1000);     
-  }
-  
-  chrome.runtime.onStartup.addListener(function() {
-    start();
-  })
+    }
+  });
+}
+
+function start() {
+  setInterval(check, 1000);
+}
+
+chrome.runtime.onStartup.addListener(function () {
   start();
+})
+start();
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "toggleBlock") {
+    shouldBlock = message.shouldBlock;
+    console.log(shouldBlock);
+  }
+});
