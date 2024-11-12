b3e1437fc4a2d8d1a68fd580738cbabdd3eb3ed5
RayanEreteo <rayabf5@gmail.com>
2024-11-12 15:46:27 +0100

    changement localstorage en fonction de la checkbox

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
-let blockStatusText = document.querySelector("#block-status")
-let blockCheckbox = document.querySelector("#should-block")
+let blockStatusText = document.querySelector("#block-status");
+let blockCheckbox = document.querySelector("#should-block");
+let checked = true;
 
-if(blockCheckbox){
-  blockCheckbox.addEventListener("change", (e) => {
-    chrome.runtime.sendMessage({ action: "toggleBlock", shouldBlock: e.target.checked });
-  })
+chrome.storage.local.get(["key"], (result) => {
+  console.log(result.key);
+  
+  checked = result.key;
+  
+  blockCheckbox.checked = checked;
+  
+  if(blockCheckbox){
+    blockCheckbox.addEventListener("change", (e) => {
+      chrome.runtime.sendMessage({ action: "toggleBlock", shouldBlock: result.key });
+      updateLocalStorage()
+    });
+  }
+});
+
+function updateLocalStorage(){
+  chrome.storage.local.set({ key: blockCheckbox.checked}, function () {
+    console.log('Value is set to : ' + blockCheckbox.checked);
+  });
 }
