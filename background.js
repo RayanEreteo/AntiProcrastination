44dd6e22e864b3f0a53c45934e510e3adedbdd34
RayanEreteo <rayabf5@gmail.com>
2024-11-13 17:28:03 +0100

    placeholder dynamisme

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 const forbiddenWebsites = [
-  /youtube\.com\/(.*)/,
-  /twitter\.com\/(.*)/,
-  /instagram\.com\/(.*)/,
-  /tiktok\.com\/(.*)/,
-  /facebook\.com\/(.*)/,
 ];
 
-// Verifies if the user is on a blocked website; if so, redirects.
+// Vérifie si l'utilisateur se trouve sur un site Web bloqué ; si c'est le cas, redirige.
 function check() {
   // Get the current "key" value from chrome.storage each time check() is called
   chrome.storage.local.get(["key"], (result) => {
@@ -27,21 +22,32 @@ function check() {
   });
 }
 
-// Run check() when the active tab changes
+// Exécutez check() lorsque l'onglet actif change
 chrome.tabs.onActivated.addListener(() => {
   check();
 });
 
-// Run check() when the URL of a tab is updated
+// Exécutez check() lorsque l'URL d'un onglet est mise à jour
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) {
     check();
   }
 });
 
-// Listen for changes in chrome.storage and rerun check if "key" changes
+// Écoutez les modifications dans chrome.storage et réexécutez pour vérifier si la "clé" change
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === "local" && changes.key) {
     check();
   }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const url = request.url;
+  const escapedUrl = url.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, '\\$&'); // Escapes special regex characters
+  
+  // Crée une regex qui correspond au domaine complet (début de la chaîne)
+  const regex = new RegExp('^https?://(?:www\\.)?' + escapedUrl);  // Match "http://youtube.com" or "https://youtube.com"
+  
+  forbiddenWebsites.push(regex); // Push au tableau des sites interdit
+  console.log(regex);
 });
