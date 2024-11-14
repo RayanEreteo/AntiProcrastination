a98a49cbbfbb59a9512206644d845fb42c6d93da
RayanEreteo <rayabf5@gmail.com>
2024-11-14 13:39:00 +0100

    vérifie si l'input contient un site web.

--- background.js
+++ background.js
@@ -45,6 +45,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const url = request.url
 
   if (url == "" || url == undefined || url == null) return console.log("no input")
+  if (!url.includes(".com")) return console.log("No website");
 
   const regex = RegExp(url)
 
