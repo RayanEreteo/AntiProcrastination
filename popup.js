1bf468821a0efe6da3bdb5149ea2d90ddaa3f633
RayanEreteo <rayabf5@gmail.com>
2024-11-15 18:48:24 +0100

    recuperation des sites web bannis dans le localStorage et creation des fonctions de suppr.

--- popup.js
+++ popup.js
@@ -3,9 +3,20 @@ let input = document.querySelector("#website-input");
 let button = document.querySelector("#block-button")
 let websitesList = document.querySelector("#websites-list")
 
-const forbiddenWebsites = [];
+let forbiddenWebsites = [];
 
 button.addEventListener("click", addWebsite)
+
+chrome.storage.local.get(["websites"], (result) => {
+  forbiddenWebsites = JSON.parse(result.websites)
+
+  for (let i = 0; i < forbiddenWebsites.length; i++) {
+    let p = document.createElement("p")
+
+    p.innerText = forbiddenWebsites[i]
+    websitesList.append(p)
+  }
+});
 
 chrome.storage.local.get(["key"], (result) => {
   blockCheckbox.checked = result.key;
@@ -34,17 +45,25 @@ function addWebsite() {
   chrome.storage.local.set({websites: JSON.stringify(forbiddenWebsites)}, () => {
     console.log("websites : " + JSON.stringify(forbiddenWebsites));
   })
-  chrome.storage.local.get(["websites"], (result) => {
-    console.log(result);
-  })
+
   updateWebsitesList(url)
   input.value = ""
 }
 
 function updateWebsitesList(url){
     let p = document.createElement("p")
-    
-    p.innerText = url
 
+    p.innerText = url
     websitesList.append(p)
+}
+
+
+function clearSingleWebsite(){
+ //? A faire
+}
+
+
+//! Debug uniquement (surement ?)
+function clearAllWebsitesStorage(){
+  chrome.storage.local.remove("websites")
 }
